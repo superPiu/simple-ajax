ea3e7efa8f35f96d6473abb4169cb8eaeba6c36c
Sean <lleiiell@live.com>
2021-12-26 09:27:37 +0800

    update

--- utils/ajaxNew.js
+++ utils/ajaxNew.js
@@ -1,6 +1,7 @@
-function ajax(params) {
-    function Ajax(params) {
-        this.default = {
+function ajax(param) {
+    function Ajax(param) {
+        
+        const defaultOpt = {
             method:'GET',
             url:'',
             data:null,
@@ -13,11 +14,11 @@ function ajax(params) {
             error:Function.prototype,
             complete:Function.prototype
         }
-        this.xhr = null;
-        this.params = Object.assign(this.default,params);
-        this.requestType = this.params.dataType === 'jsonp' ? 'jsonpReq' : 'xhrReq';
+        let xhr = null;
+        const params = Object.assign(defaultOpt,param);
+        const requestType = params.dataType === 'jsonp' ? 'jsonpReq' : 'xhrReq';
          //处理data入参,支持字符串形式和对象形式
-         this.formartData = function(data){
+         Ajax.prototype.formartData = function(data){
             if(typeof data === 'object'){
                 let arr = []
                 for(param in data){
@@ -30,7 +31,7 @@ function ajax(params) {
         /**
          *  @param {number} [len] 字符串长度
          */
-        this.randomString = function(len) {    
+         Ajax.prototype.randomString = function(len) {    
             len = len || 32;
             var str = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",
             strLen = str.length,
@@ -38,39 +39,39 @@ function ajax(params) {
             for (i = 0; i < len; i++) randomStr += str.charAt(Math.floor(Math.random() * strLen));
             return randomStr
         }
+        //xmlHTTPRequest 请求对象,包含创建和取消两个方法
         this.xhrReq = {
             create:function() {
-                const xhr = new XMLHttpRequest();
-                this.xhr = xhr;
+                xhr = new XMLHttpRequest();
+                //xhr = xhr;
                 //设置请求头
-                for (key in this.params.header) {
-                    xhr.setRequestHeader(key, this.params.header[key])
+                for (key in params.header) {
+                    xhr.setRequestHeader(key, params.header[key])
                 }
-                const data = this.formartData(this.params.data)
-                if(this.params.method.toLowerCase() === 'get'){
-                    this.params.url += ('?'+ data)
-                    xhr.open(this.params.method,this.params.url,this.params.async)
+                const data = this.formartData(params.data)
+                if(params.method.toLowerCase() === 'get'){
+                    params.url += ('?'+ data)
+                    xhr.open(params.method,params.url,params.async)
                     xhr.send();
                 }else{
-                    xhr.open(this.params.method,this.params.url,this.params.async)
+                    xhr.open(params.method,params.url,params.async)
                     // 请求的请求头
                     xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
                     xhr.send(data);
                 }
-                const that = this
                 xhr.onreadystatechange = function(){
                     if(xhr.readyState !== 4) return;
-                    typeof that.params.complete === 'function' && that.params.complete(xhr,xhr.statusText) 
+                    typeof params.complete === 'function' && params.complete(xhr,xhr.statusText) 
                     if(xhr.status >= 200 && xhr.status <400){
                         let data = xhr.responseText;
-                        if(that.params.dataType=="json"){
+                        if(params.dataType=="json"){
                             data=JSON.parse(data);
                         } 
-                        that.params.success(data)
+                        params.success(data)
                         
                     }else{
-                        if(typeof that.params.error === "function"){
-                            that.params.error(xhr.status)
+                        if(typeof params.error === "function"){
+                            params.error(xhr.status)
                         }else{
                             console.log('请求出错~')
                         }
@@ -78,41 +79,39 @@ function ajax(params) {
                 }
             },
             abort:function () {
-                this.xhr.abort()
+                xhr.abort()
             }
         }
+        //jsonp 请求对象,包含创建和取消两个方法
         this.jsonpReq = {
             create:function () {
                 const script = document.createElement('script');
-                const data = this.formartData(this.params.data)
+                const data = this.formartData(params.data)
                 const funNameSuccess = this.randomString(10)
-                const that = this;
                 window[funNameSuccess] = function(data){
-                    that.params.success(data);
+                    params.success(data);
                 }
-                script.src = `${this.params.url}?callback=${funNameSuccess}&${data}`;
+                script.src = `${params.url}?callback=${funNameSuccess}&${data}`;
                 document.body.appendChild(script);
             },
             abort:function(){
-                
+                params.success = null;
             }
         }
-        if(this.params.delay > 0){
+        //延时发起请求
+        if(params.delay > 0){
             setTimeout(function() {
-                this[this.requestType].create.call(this)
-            },this.params.delay)
+                this[requestType].create.call(this)
+            },params.delay)
         }else{
-            console.log(this.requestType)
-            this[this.requestType].create.call(this)
+            this[requestType].create.call(this)
         }
+        //终止请求
         this.abort = function () {
-            this.requestType.abort()
-        }
-        this.sendRequest = function () {
-            
+            this[requestType].abort()
         }
         
        
     }
-    return new Ajax(params)
+    return new Ajax(param)
 }
